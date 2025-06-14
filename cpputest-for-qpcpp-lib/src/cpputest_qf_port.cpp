f70294889bbb7fc79b71006402c264c3f4002a42
Matthew Eshleman <matthew@covemountainsoftware.com>
2025-06-14 09:39:39 -0500

    prepare for QP v8.x.x

--- cpputest-for-qpcpp-lib/src/cpputest_qf_port.cpp
+++ cpputest-for-qpcpp-lib/src/cpputest_qf_port.cpp
@@ -6,11 +6,11 @@
 /// @endcond
 ///
 
-#define QP_IMPL             // this is QP implementation
-#include "qp_port.hpp"      // QF port
-#include "qp_pkg.hpp"       // QF package-scope interface
-#include "qsafe.h"        // QP embedded systems-friendly assertions
-#ifdef Q_SPY                // QS software tracing enabled?
+#define QP_IMPL          // this is QP implementation
+#include "qp_port.hpp"   // QF port
+#include "qp_pkg.hpp"    // QF package-scope interface
+#include "qsafe.h"       // QP embedded systems-friendly assertions
+#ifdef Q_SPY             // QS software tracing enabled?
     #error "Q_SPY not supported in the cpputest port"
 #else
     #include "qs_dummy.hpp"   // disable the QS software tracing
@@ -39,25 +39,42 @@ int_t QF::run()
 }
 #endif
 
+/**
+ * Fake cpputest event loop for QActive. Only loops
+ * until queue is empty, then removes it from the
+ * ready set.
+ * @param act an active object to run an event loop upon.
+ */
+void QActive::evtLoop_(QActive* act)
+{
+    while (!act->m_eQueue.isEmpty()) {
+        QEvt const* e = act->get_();
+        act->dispatch(e, act->m_prio);
+        QF::gc(e);
+    }
+
+    if (act->m_eQueue.isEmpty()) {
+        cpputest_readySet_.remove(act->m_prio);
+    }
+}
+
 void RunUntilNoReadyActiveObjects()
 {
     while (cpputest_readySet_.notEmpty()) {
         std::uint_fast8_t p = cpputest_readySet_.findMax();
+        #if QP_VERSION > 800
+        QActive* a          = QP::QActive::fromRegistry(p);
+        #elif QP_VERSION > 700
         QActive* a          = QP::QActive::registry_[p];
+        #else
+        #error "unsupported QP version"
+        #endif
 
         // the active object 'a' must still be registered in QF
         // (e.g., it must not be stopped)
         Q_ASSERT_ID(320, a != nullptr);
 
-        while (!a->getEQueue().isEmpty()) {
-            const auto e = a->get_();
-            a->dispatch(e, 0);
-            QF::gc(e);
-        }
-
-        if (a->getEQueue().isEmpty()) { /* empty queue? */
-            cpputest_readySet_.remove(p);
-        }
+        QActive::evtLoop_(a);
     }
 }
 
@@ -68,23 +85,22 @@ void QF::stop()
 }
 
 //****************************************************************************
-void QActive::start( QPrioSpec const prioSpec,
-                    QEvt const * * const qSto, std::uint_fast16_t const qLen,
-                    void * const stkSto, std::uint_fast16_t const stkSize,
-                    void const * const par)
+void QActive::start(QPrioSpec const prioSpec, QEvt const** const qSto,
+                    std::uint_fast16_t const qLen, void* const stkSto,
+                    std::uint_fast16_t const stkSize, void const* const par)
 {
     // unused parameters in the cpputest port
     Q_UNUSED_PAR(stkSto);
     Q_UNUSED_PAR(stkSize);
 
-    m_prio  = static_cast<std::uint8_t>(prioSpec & 0xFFU); // QF-priority
-    m_pthre = static_cast<std::uint8_t>(prioSpec >> 8U); // preemption-thre.
-    register_(); // make QF aware of this AO
+    m_prio  = static_cast<std::uint8_t>(prioSpec & 0xFFU);   // QF-priority
+    m_pthre = static_cast<std::uint8_t>(prioSpec >> 8U);     // preemption-thre.
+    register_();   // make QF aware of this AO
 
     m_eQueue.init(qSto, qLen);
 
-    this->init(par, m_prio); // execute initial transition (virtual call)
-    QS_FLUSH(); // flush the QS trace buffer to the host
+    this->init(par, m_prio);   // execute initial transition (virtual call)
+    QS_FLUSH();                // flush the QS trace buffer to the host
 }
 
 //............................................................................
